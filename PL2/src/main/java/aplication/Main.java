a18f752bd3f530d8272942c7ad748e2b8b546240
arceniouate <arcenio.uate@gmail.com>
2020-03-02 22:31:08 +0000

    pl2

---
+++ PL2/src/main/java/aplication/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplication;
+
+import entites.Data;
+
+/**
+ *
+ * @author arccenio
+ */
+public class Main {
+    public static void main(String[] args) {
+            Data l=new  Data();
+
+    }
+    
+}
